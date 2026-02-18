48fa8ed000ad7bec868f933dc9699583e079f189
AbhiiisheK990 <97405078+AbhiiisheK990@users.noreply.github.com>
2026-02-19 01:23:39 +0530

    Clean commit without secrets

--- llm.py
+++ llm.py
@@ -1,5 +1,5 @@
 from openai import OpenAI
-client = OpenAI(api_key="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")
+client = OpenAI(api_key="YOUR_OPENAI_API_KEY")
 
 def ask_llm(prompt):
     response = client.chat.completions.create(
